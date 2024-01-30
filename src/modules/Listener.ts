800a4c652e4953f82b3b769974a8a44db465a3c3
ZeroOneDeveloper <me@zero0ne.dev>
2024-01-30 17:38:59 +0900

    feat: notify warning channels func

--- src/modules/Listener.ts
+++ src/modules/Listener.ts
@@ -1,4 +1,5 @@
 import {
+  Embed,
   GuildMember,
   GuildMemberRoleManager,
   Interaction,
@@ -25,7 +26,6 @@ class Listener extends Extension {
       ],
     });
     const CHECK_CHANNELS = async () => {
-      const punishedChannels: string[] = [];
       const GUILD = this.client.guilds.cache.get(process.env.GUILD!);
       if (!GUILD) return;
       const CHANNELS = GUILD.channels.cache.filter(
@@ -34,53 +34,55 @@ class Listener extends Extension {
           x.parent?.name === "🎨【 작가채널 】" &&
           x.name.endsWith("작가")
       );
-      CHANNELS.map(async (channel) => {
-        const punishmentTime = new Date();
-        punishmentTime.setHours(punishmentTime.getHours() + 9);
-        const lastMessage = (
-          await (channel as TextChannel).messages.fetch({ limit: 1 })
-        ).last();
-        if (!lastMessage) {
-          const channelCreatedAt = (channel as TextChannel).createdAt as Date;
-          punishmentTime.setDate(channelCreatedAt.getDate() + 14);
-          punishmentTime.setHours(channelCreatedAt.getHours() + 9);
-          punishmentTime.setMinutes(channelCreatedAt.getMinutes());
-          punishmentTime.setSeconds(channelCreatedAt.getSeconds());
-        } else {
-          const lastMessageCreatedAt = lastMessage.createdAt as Date;
-          lastMessageCreatedAt.setHours(lastMessageCreatedAt.getHours() + 9);
-          punishmentTime.setHours(lastMessageCreatedAt.getHours());
-          punishmentTime.setMinutes(lastMessageCreatedAt.getMinutes());
-          punishmentTime.setSeconds(lastMessageCreatedAt.getSeconds());
-          if (lastMessage.author.id === this.client.user?.id) {
-            punishmentTime.setDate(lastMessageCreatedAt.getDate() + 1);
+      const punishedChannels = await Promise.all(
+        CHANNELS.map(async (channel) => {
+          const punishmentTime = new Date();
+          punishmentTime.setHours(punishmentTime.getHours() + 9);
+          const lastMessage = (
+            await (channel as TextChannel).messages.fetch({ limit: 1 })
+          ).last();
+          if (!lastMessage) {
+            const channelCreatedAt = (channel as TextChannel).createdAt as Date;
+            punishmentTime.setDate(channelCreatedAt.getDate() + 14);
+            punishmentTime.setHours(channelCreatedAt.getHours() + 9);
+            punishmentTime.setMinutes(channelCreatedAt.getMinutes());
+            punishmentTime.setSeconds(channelCreatedAt.getSeconds());
           } else {
-            punishmentTime.setDate(lastMessageCreatedAt.getDate() + 14);
+            const lastMessageCreatedAt = lastMessage.createdAt as Date;
+            lastMessageCreatedAt.setHours(lastMessageCreatedAt.getHours() + 9);
+            punishmentTime.setHours(lastMessageCreatedAt.getHours());
+            punishmentTime.setMinutes(lastMessageCreatedAt.getMinutes());
+            punishmentTime.setSeconds(lastMessageCreatedAt.getSeconds());
+            if (lastMessage.author.id === this.client.user?.id) {
+              punishmentTime.setDate(lastMessageCreatedAt.getDate() + 1);
+            } else {
+              punishmentTime.setDate(lastMessageCreatedAt.getDate() + 14);
+            }
           }
-        }
-        if (new Date() > punishmentTime) {
-          if (!punishedChannels.includes(channel.id.toString())) {
-            punishedChannels.push(channel.id.toString());
+          if (new Date() > punishmentTime) {
+            return channel.id;
           }
-        }
+        })
+      );
+      await (
+        GUILD.channels.cache.get("996824873009680425") as TextChannel
+      ).send({
+        embeds: [
+          {
+            title: "⚠️ 채널 처리 요청",
+            description: punishedChannels
+              .filter((x) => {
+                return x !== undefined;
+              })
+              .map((x) => `<#${x}>`)
+              .join("\n"),
+            color: 0xffa500,
+          },
+        ],
       });
-      if (punishedChannels.sort() !== punishedChannels.sort()) {
-        await (
-          this.client.channels.cache.get("996824873009680425") as TextChannel
-        ).send({
-          embeds: [
-            {
-              title: "⚠️ 경고 조치 요망",
-              description: punishedChannels.map((x) => `<#${x}>`).join("\n"),
-              color: 0xff9900,
-            },
-          ],
-        });
-        data = punishedChannels;
-      }
     };
     await CHECK_CHANNELS();
-    setInterval(CHECK_CHANNELS, 1000 * 60);
+    setInterval(CHECK_CHANNELS, 1000 * 60 * 60 * 12);
   }
 
   @listener({ event: "applicationCommandInvokeError", emitter: "cts" })
