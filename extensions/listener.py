0289301ab8ae52b54e89d5743a1450b0c6c7fe1f
ZeroOneDeveloper <me@zero0ne.dev>
2024-02-27 22:16:04 +0900

    feat: check artist channel algorithm

--- extensions/listener.py
+++ extensions/listener.py
@@ -1,26 +1,27 @@
-from discord import CategoryChannel
 from discord.ext import commands, tasks
+from discord import Embed, Color, CategoryChannel
 
 from main import ART
 
 import os
 from typing import List
 from pytz import timezone
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 
 class Listener(commands.Cog):
     def __init__(self, bot: ART):
         self.bot = bot
+        self.logger = bot.logger
         self.database = bot.database
         self.checkArtistChannel.start()
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        print(f"Logged in as {self.bot.user} ({self.bot.user.id})")
+    async def on_ready(self) -> None:
+        self.logger.info(f"Logged in as {self.bot.user} ({self.bot.user.id})")
 
-    @tasks.loop(seconds=10)
-    async def checkArtistChannel(self):
+    @tasks.loop(seconds=60)
+    async def checkArtistChannel(self) -> None:
         GUILD = self.bot.get_guild(int(os.getenv("GUILD")))
         if GUILD is None:
             return
@@ -35,15 +36,10 @@ class Listener(commands.Cog):
                 if channel.name == "📢│중대발표":
                     continue
                 messages = [message async for message in channel.history(limit=2)]
-                print(
-                    messages[0]
-                    .created_at.replace(tzinfo=timezone("UTC"))
-                    .astimezone(timezone("Asia/Seoul"))
-                )
                 if len(messages) == 0:
                     punishedTime = channel.created_at.replace(
                         tzinfo=timezone("UTC")
-                    ).astimezone(timezone("Asia/Seoul"))
+                    ).astimezone(timezone("Asia/Seoul")) + timedelta(days=14)
                 else:
                     if messages[0].author.bot:
                         if len(messages) == 1:
@@ -57,8 +53,46 @@ class Listener(commands.Cog):
                     else:
                         punishedTime = messages[0].created_at.replace(
                             tzinfo=timezone("UTC")
-                        ).astimezone(timezone("Asia/Seoul")) + timedelta(days=1)
-
+                        ).astimezone(timezone("Asia/Seoul")) + timedelta(days=14)
+                if datetime.now().replace(tzinfo=timezone("Asia/Seoul")) > punishedTime:
+                    punishedChannels.append(channel)
+        noticedChannels = []
+        for punishedChannel in punishedChannels:
+            data = await self.database["artists"].find_one(
+                {"channelId": str(punishedChannel.id)}
+            )
+            if len(data["punished"]) > 3:
+                noticedChannels.append(punishedChannel)
+            else:
+                await self.database["artists"].update_one(
+                    {"channelId": str(punishedChannel.id)},
+                    {
+                        "$push": {
+                            "punished": datetime.now()
+                            .replace(tzinfo=timezone("Asia/Seoul"))
+                            .strftime('%Y년 %m월 %d일 %H시 %M분 %S초')
+                        }
+                    },
+                )
+                await punishedChannel.send(
+                    embed=Embed(
+                        title='⚠️ 경고',
+                        description='장기간 미활동으로 24시간 후 채널 삭제합니다!\n'
+                                    '그림을 올리시면 보존되니 참고바랍니다!\n'
+                                    '`이 뒤로는 적어도 14일에 한번씩은 활동 부탁드려요!`',
+                        color=Color.red()
+                    )
+                )
+        if len(noticedChannels) > 0:
+            channels = "\n".join([channel.mention for channel in noticedChannels])
+            await GUILD.get_channel(int(os.getenv("MANAGE_CHANNEL"))).send(
+                embed=Embed(
+                    title='⚠️ 채널 처리 요청',
+                    description=f'🚨 미활동 채널 {len(noticedChannels)}개 발견됨\n'
+                                f'📌\n{channels}',
+                    color=Color.orange()
+                )
+            )
 
 
 async def setup(bot: ART):
